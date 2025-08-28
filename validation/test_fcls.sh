07a1ddf96c3d9128320dd2f6525838d53b59f750
Andrew Edmonds <aedmonds@york.cuny.edu>
2025-08-28 15:30:06 -0500

    Print the actual validation command run to the output file

--- validation/test_fcls.sh
+++ validation/test_fcls.sh
@@ -31,7 +31,8 @@ done
 
 
 echo -n "from_mcs-mockdata.fcl... "
-mu2e -c fcl/from_mcs-mockdata.fcl -S ../filelists/$mock_dataset.list --TFileName nts.ntuple.mock.root -n 100 > ${log_file} 2>&1
+echo "mu2e -c fcl/from_mcs-mockdata.fcl -S ../filelists/$mock_dataset.list --TFileName nts.ntuple.mock.root -n 100" > ${log_file} 2>&1
+mu2e -c fcl/from_mcs-mockdata.fcl -S ../filelists/$mock_dataset.list --TFileName nts.ntuple.mock.root -n 100 >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
 else
@@ -39,6 +40,7 @@ else
 fi
 
 echo -n "from_mcs-mockdata_noMC.fcl... "
+echo "mu2e -c fcl/from_mcs-mockdata_noMC.fcl -S ../filelists/$mock_dataset.list --TFileName nts.ntuple.mockNoMC.root -n 100" >> ${log_file} 2>&1
 mu2e -c fcl/from_mcs-mockdata_noMC.fcl -S ../filelists/$mock_dataset.list --TFileName nts.ntuple.mockNoMC.root -n 100 >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
@@ -47,6 +49,7 @@ else
 fi
 
 echo -n "from_mcs-primary.fcl... "
+echo "mu2e -c fcl/from_mcs-primary.fcl -S ../filelists/$primary_dataset.list --TFileName nts.ntuple.primary.root -n 100" >> ${log_file} 2>&1
 mu2e -c fcl/from_mcs-primary.fcl -S ../filelists/$primary_dataset.list --TFileName nts.ntuple.primary.root -n 100 >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
@@ -55,6 +58,7 @@ else
 fi
 
 echo -n "from_mcs-mixed.fcl... "
+echo "mu2e -c fcl/from_mcs-mixed.fcl -S ../filelists/$mixed_dataset.list --TFileName nts.ntuple.mixed.root -n 100" >> ${log_file} 2>&1
 mu2e -c fcl/from_mcs-mixed.fcl -S ../filelists/$mixed_dataset.list --TFileName nts.ntuple.mixed.root -n 100 >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
@@ -63,6 +67,7 @@ else
 fi
 
 echo -n "from_mcs-extracted.fcl... "
+echo "mu2e -c fcl/from_mcs-extracted.fcl -S ../filelists/$extracted_dataset.list --TFileName nts.ntuple.extracted.root -n 100" >> ${log_file} 2>&1
 mu2e -c fcl/from_mcs-extracted.fcl -S ../filelists/$extracted_dataset.list --TFileName nts.ntuple.extracted.root -n 100 >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
@@ -71,7 +76,9 @@ else
 fi
 
 echo -n "from_mcs-ceSimReco.fcl... "
+echo "mu2e -c Production/Validation/ceSimReco.fcl -n 10" >> ${log_file} 2>&1
 mu2e -c Production/Validation/ceSimReco.fcl -n 10 >> ${log_file} 2>&1
+echo "mu2e -c fcl/from_mcs-ceSimReco.fcl -s mcs.owner.val-ceSimReco.dsconf.seq.art --TFileName nts.ntuple.ceSimReco.root" >> ${log_file} 2>&1
 mu2e -c fcl/from_mcs-ceSimReco.fcl -s mcs.owner.val-ceSimReco.dsconf.seq.art --TFileName nts.ntuple.ceSimReco.root >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
@@ -80,6 +87,7 @@ else
 fi
 
 echo -n "from_mcs-mockdata_separateTrkBranches.fcl... "
+echo "mu2e -c fcl/from_mcs-mockdata_separateTrkBranches.fcl -S ../filelists/${mock_dataset}.list --TFileName nts.ntuple.mockSepTrkBranches.root -n 100" >> ${log_file} 2>&1
 mu2e -c fcl/from_mcs-mockdata_separateTrkBranches.fcl -S ../filelists/${mock_dataset}.list --TFileName nts.ntuple.mockSepTrkBranches.root -n 100 >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
@@ -88,6 +96,7 @@ else
 fi
 
 echo -n "from_mcs-mockdata_selectorExample.fcl... "
+echo"mu2e -c fcl/from_mcs-mockdata_selectorExample.fcl -S ../filelists/${mock_dataset}.list --TFileName nts.ntuple.mockSelector.root -n 100" >> ${log_file} 2>&1
 mu2e -c fcl/from_mcs-mockdata_selectorExample.fcl -S ../filelists/${mock_dataset}.list --TFileName nts.ntuple.mockSelector.root -n 100 >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
@@ -96,6 +105,7 @@ else
 fi
 
 echo -n "from_dig-mockdata.fcl... "
+echo "mu2e -c fcl/from_dig-mockdata.fcl -S ../filelists/${digi_dataset}.list --TFileName nts.ntuple.dig.root -n 100" >> ${log_file} 2>&1
 mu2e -c fcl/from_dig-mockdata.fcl -S ../filelists/${digi_dataset}.list --TFileName nts.ntuple.dig.root -n 100 >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
@@ -104,6 +114,7 @@ else
 fi
 
 echo -n "creating file for validation script... "
+echo "mu2e -c fcl/from_mcs-mockdata.fcl -S ../filelists/${mock_dataset}.list --TFileName nts.ntuple.after.root -n 100" >> ${log_file} 2>&1
 mu2e -c fcl/from_mcs-mockdata.fcl -S ../filelists/${mock_dataset}.list --TFileName nts.ntuple.after.root -n 100 >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
@@ -112,6 +123,7 @@ else
 fi
 
 echo -n "creating validation file... "
+echo "root -l -b -q validation/create_val_file_rooutil.C\(\"nts.ntuple.after.root\",\"val.ntuple.after.root\"\)" >> ${log_file} 2>&1
 root -l -b -q validation/create_val_file_rooutil.C\(\"nts.ntuple.after.root\",\"val.ntuple.after.root\"\) >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
@@ -120,6 +132,7 @@ else
 fi
 
 echo -n "from_rec-crv-vst.fcl... "
+echo "mu2e -c fcl/from_rec-crv-vst.fcl -S ../filelists/${crv_vst_dataset}.list --TFileName nts.ntuple.crv-vst.root -n 100" >> ${log_file} 2>&1
 mu2e -c fcl/from_rec-crv-vst.fcl -S ../filelists/${crv_vst_dataset}.list --TFileName nts.ntuple.crv-vst.root -n 100 >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
@@ -128,6 +141,7 @@ else
 fi
 
 echo -n "from_mcs-DeCalib.fcl... "
+echo "mu2e -c fcl/from_mcs-DeCalib.fcl -S ../filelists/${mixed_dataset}.list --TFileName nts.ntuple.deCalib.root -n 100" >> ${log_file} 2>&1
 mu2e -c fcl/from_mcs-DeCalib.fcl -S ../filelists/${mixed_dataset}.list --TFileName nts.ntuple.deCalib.root -n 100 >> ${log_file} 2>&1
 if [ $? == 0 ]; then
     echo "OK"
