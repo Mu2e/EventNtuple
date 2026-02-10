d4ea66330af3ed78a8f90e4434200c102d6d0235
Andrew Edmonds <aedmonds@york.cuny.edu>
2026-02-10 10:13:50 -0600

    Update version number

--- src/EventNtupleMaker_module.cc
+++ src/EventNtupleMaker_module.cc
@@ -454,7 +454,7 @@ namespace mu2e {
     _hVersion = tfs->make<TH1I>("version", "version number",3,0,3);
     _hVersion->GetXaxis()->SetBinLabel(1, "major"); _hVersion->SetBinContent(1, 6);
     _hVersion->GetXaxis()->SetBinLabel(2, "minor"); _hVersion->SetBinContent(2, 9);
-    _hVersion->GetXaxis()->SetBinLabel(3, "patch"); _hVersion->SetBinContent(3, 1);
+    _hVersion->GetXaxis()->SetBinLabel(3, "patch"); _hVersion->SetBinContent(3, 2);
     // add event info branch
     _ntuple->Branch("evtinfo",&_einfo,_buffsize,_splitlevel);
     if (_fillmc) {
